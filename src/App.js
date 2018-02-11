898c9959ad9bf83ca4808ed1e225ecf05d623aee
Kat Gifford <katgiff2007@gmail.com>
2018-02-11 12:29:45 -0800

    connect up firebase api and add .env file to keep apikeys secure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Header } from './components/common';
 import firebase from 'firebase';
 
+import { Header } from './components/common';
+
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+      authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+      projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.REACT_APP_FIREBASE_MESSEGE_SENDER_ID
+    });
+  }
+
   render () {
     return (
       <View>
-        <Header headerText="Authentication" />
+        <Header headerText='Authentication' />
         <Text>Auth App</Text>
       </View>
     );
