4f8faadb0a92d234e1135d90461d0840b0a7d580
Kat Gifford <katgiff2007@gmail.com>
2018-02-11 11:15:47 -0800

    add content and tell index to load App component

--- src/App.js
+++ src/App.js
@@ -1,2 +1,14 @@
-import React { Component } from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+
+class App extends Component {
+  render () {
+    return (
+      <View>
+        <Text>Auth App</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
