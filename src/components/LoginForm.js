0454213b0728254222331bf26a3167ba3a6c4d96
Kat Gifford <katgiff2007@gmail.com>
2018-02-11 12:36:05 -0800

    add the reusable components to the LoginForm components

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 
+import { Button , Card, CardSection } from './common';
+
 class LoginForm extends Component {
   render() {
     return (
-      <View />
+      <Card>
+        <CardSection />
+        <CardSection />
+
+        <CardSection>
+          <Button>
+            Login
+          </Button>
+        </CardSection>
+      </Card>
     );
   }
 }
