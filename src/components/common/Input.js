8b2b00ef0852d14de085de9722110cdde9140a4a
Kat Gifford <katgiff2007@gmail.com>
2018-02-11 18:16:12 -0800

    set up styles for the deconstructed parts of the Input Components

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { TextInput, View, Text } from 'react-native';
 
 const Input = ({ label, value, onChangeText }) => {
+  const { inputStyle, labelStyle, containerStyle } = styles;
+
   return (
-    <View>
-      <Text>{label}</Text>
+    <View style={containerStyle}>
+      <Text style={labelStyle}>{label}</Text>
       <TextInput
+        style={inputStyle}
         value={value}
         onChangeText={onChangeText}
         style={{ height:20, width:100 }}
@@ -13,5 +16,27 @@ const Input = ({ label, value, onChangeText }) => {
     </View>
   );
 };
+
+const styles = {
+  inputStyle: {
+    flex: 2,
+    color: '#a1a1a1',
+    lineHeight: 23,
+    fontSize: 18,
+    paddingRight: 5,
+    paddingLeft: 5
+  },
+  labelStyle: {
+    flex: 1,
+    fontSize: 18,
+    paddingLeft: 20
+  },
+  containerStyle: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 40
+  }
+}
 
 export { Input };
