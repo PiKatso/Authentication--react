c4e0a7e79d8f3f80190cd921c742b972c3a093b3
Kat Gifford <katgiff2007@gmail.com>
2018-02-11 19:00:56 -0800

    add firebase create and error catching, add error state and error state style to LoginForm Component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
 import firebase from 'firebase';
 
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-  state = { email : '', password: '' };
+  state = { email : '', password: '', error: '' };
 
   onButtonPress() {
     const { email, password } = this.state;
 
-    firebase.auth().signInWithEmailAndPassword(email, password);
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .catch(() => {
+            this.setState({error: 'Authentication Failed.'});
+          });
+      });
   }
 
   render() {
@@ -34,6 +41,10 @@ class LoginForm extends Component {
           />
         </CardSection>
 
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
+
         <CardSection>
           <Button onPress={this.onButtonPress.bind(this)}>
             Login
@@ -41,6 +52,14 @@ class LoginForm extends Component {
         </CardSection>
       </Card>
     );
+  }
+}
+
+const styles ={
+  errorTextStyle: {
+    color: 'red',
+    alignSelf: 'center',
+    fontSize: 20
   }
 }
 
