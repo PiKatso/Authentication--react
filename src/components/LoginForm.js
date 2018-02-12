d72f70d23a633b82b18f7181e5aa2ffd8324f9a1
Kat Gifford <katgiff2007@gmail.com>
2018-02-11 18:28:53 -0800

    refactor in prep for additional props to be passed/a more specific state title

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-  state = { text : '' };
+  state = { email : '' };
 
   render() {
     return (
@@ -12,8 +12,8 @@ class LoginForm extends Component {
           <Input
             label="Email"
             placeholder="user@gmail.com"
-            value={this.state.text}
-            onChangeText={text => this.setState({ text })}
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
           />
         </CardSection>
 
