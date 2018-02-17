61894b5fa2a8d5f57798457bcfd4d301a5094a79
Kat Gifford <katgiff2007@gmail.com>
2018-02-17 14:52:03 -0800

    add firebase listener to handle login state change and begin the process of changing user sign in view

--- src/App.js
+++ src/App.js
@@ -8,8 +8,19 @@ import LoginForm from './components/LoginForm';
 import { REACT_NATIVE_Config }from '../config.env';
 
 class App extends Component {
+  state = { loggedIn: false };
+
   componentWillMount() {
     firebase.initializeApp(REACT_NATIVE_Config);
+
+    //this will watch the firebase auth state change and change user loggedIn state accordingly
+    firebase.auth().onAuthStateChange((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
   }
 
   render () {
