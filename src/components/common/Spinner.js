1d233f07d4727cb935170b959dcefd468a5f24de
Kat Gifford <katgiff2007@gmail.com>
2018-02-17 13:10:56 -0800

    add ActivityIndicator for react-native library and basic styles to center and take full screen

--- src/components/common/Spinner.js
+++ src/components/common/Spinner.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ActivityIndictor } from 'react-native';
 
 const Spinner = () => {
   return (
-    <View />
+    <View style={styles.spinnerStyle}>
+      <ActivityIndictor />
+    </View>
   );
+};
+
+const styles ={
+  spinnerStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItemss: 'center'
+  }
 }
 
 export { Spinner };
